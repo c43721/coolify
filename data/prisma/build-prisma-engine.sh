15d5dfbbb8c2cbe606d04d4d2fe4a5548209d856
Andras Bacsai <andras.bacsai@gmail.com>
2022-06-22 11:12:23 +0200

    fix: build script

--- data/prisma/build-prisma-engine.sh
+++ data/prisma/build-prisma-engine.sh
@@ -1,1 +1,1 @@
-docker build --platform linux/amd64,linux/arm64 -t coollabsio/prisma-engine:3.15 -f prisma-engine.Dockerfile --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t coollabsio/prisma-engine:3.15 -f prisma-engine.Dockerfile --push .
